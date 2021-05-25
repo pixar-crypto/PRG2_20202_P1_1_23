846b51e41a2fc4c1386fa6c88319f008b285f0c2
Rifqih Syahrial Anwar <0320200023@polman.astra.ac.id>
2021-05-25 16:23:35 +0700

    Add project files.

---
+++ PRG2_20202_P1_1_23/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRG2_20202_P1_1_23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World from Visual Studio, I'm 0320200023!");
+        }
+    }
+}
